cc842b9004bb4e33c9052b209640e811be701de6
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-17 12:07:31 -0300

    Update to use http.StatusCreated constant

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func snippetCreate(w http.ResponseWriter, _ *http.Request) {
 
 // snippetCreatePost processes the post request to create a new snippet.
 func snippetCreatePost(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(201)
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet"))
 }
 
