7b1d6ef6b3e3714c37810e5c119b317c9efb4d1f
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-21 10:48:58 -0300

    Add route to static and use them in tmpl

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,10 @@ func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
