c5acba1cfe7ceace04f2f192029ad2d7f5f94a0d
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-22 10:39:27 -0300

    Remove unnecessary comments in main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -18,15 +16,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
 
-	// Use the http.ListenAndServe() function to start a new web server.
-	// We pass in two parameters: the TCP network address to listen on (in
-	// this case ":4000") and the servemux we just created. If
-	// http.ListenAndServe() returns an error we use the log.Fatal()
-	// function to log the error message and exit. Note that any error
-	// returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(":4000", mux)
 
 	log.Fatal(err)
