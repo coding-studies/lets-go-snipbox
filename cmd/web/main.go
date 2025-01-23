f0413504fca11c93409c33121a559af986afd3f4
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-23 08:32:53 -0300

    Add command line flag for -addr

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Take command line argument `-addr`. E.g.:
+	//
+	//   $ go run ./cmd/web -addr=":4004"
+	//   $ go run ./cmd/web -addr=":80"
+	//
+	// Uses ":4000" that cmdline option -addr is not provided.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -16,9 +26,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	log.Print("starting server on " + *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 
 	log.Fatal(err)
 }
