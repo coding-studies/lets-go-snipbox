618d7d401a7aa261ac63cfdee1c18a8831b08c0e
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-29 11:16:12 -0300

    Add literal newlines to example SQL snippets

--- docker/docker-entrypoint-initdb.d/01.sql
+++ docker/docker-entrypoint-initdb.d/01.sql
@@ -84,17 +84,30 @@ INSERT INTO snippets (
   , expires
 ) VALUES (
     'An old silent pond'
-  , 'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō'
+  , 'An old silent pond...
+A frog jumps into the pond,
+splash!
+Silence again.
+
+– Matsuo Bashō'
   , CURRENT_TIMESTAMP
   , CURRENT_TIMESTAMP + INTERVAL '1 YEAR'
 ), (
      'Over the wintry forest'
-   , 'Over the wintry forest\n,winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki'
+   , 'Over the wintry forest,
+winds howl in rage
+with no leaves to blow.
+
+– Natsume Soseki'
    , CURRENT_TIMESTAMP
    , CURRENT_TIMESTAMP + INTERVAL '1 YEAR'
 ), (
      'First autumn morning'
-   , 'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo'
+   , 'First autumn morning
+the mirror I stare into
+shows my father''s face.
+
+– Murakami Kijo'
    , CURRENT_TIMESTAMP
    , CURRENT_TIMESTAMP + INTERVAL '1 YEAR'
 );
