2749678fb5aa616a005239a6698ad19f1254daec
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-30 11:17:52 -0300

    Add template caching function
    This change introduces a new function to prepare templates and keep them in an
    in-memory cache. This way, every new request + render will not require the
    application to read files on disk and prepare the templates over and over again.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,10 @@
 package main
 
-import "snipbox.fernandobasso.dev/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+	"snipbox.fernandobasso.dev/internal/models"
+)
 
 // templateData is used as a type-safe way to pass multiple pieces of data
 // to templates (since templates can only take a single variable containing
@@ -11,4 +15,32 @@ type templateData struct {
 
 	// For displaying a list of snippets.
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./cmd/web/ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		baseName := filepath.Base(page)
+
+		files := []string{
+			"./cmd/web/ui/html/base.tmpl.html",
+			"./cmd/web/ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[baseName] = ts
+	}
+
+	return cache, nil
 }
