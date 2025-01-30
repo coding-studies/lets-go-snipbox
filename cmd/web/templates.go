741e95c3e1426cb4631aa49fd836097480d0cc23
Fernando Basso <fernandobasso.br@gmail.com>
2025-01-30 11:38:21 -0300

    Improve the implementation of the template cache
    This change automatically parses all pages and all partials using globs. This
    way, if new partials or pages are added to their respective directories, no
    changes will be necessary and the application will simply start parsing those
    new files automatically, so, there is no chance that someone will add a new
    template but then forget to add it to some list of known templates that must be
    parsed.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -17,7 +17,13 @@ type templateData struct {
 	Snippets []models.Snippet
 }
 
+// newTemplateCache returns an in-memory cache of template sets.
+//
+// To retrieve a template from the cache we must provide the basename of the
+// page template we want to use, like "home.tmpl.html" or "view.tmpl.html".
 func newTemplateCache() (map[string]*template.Template, error) {
+	// The key is the basename of the template file, for example "home.tmpl.html"
+	// or "view.tmpl.html"
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./cmd/web/ui/html/pages/*.tmpl.html")
@@ -28,17 +34,25 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		baseName := filepath.Base(page)
 
-		files := []string{
-			"./cmd/web/ui/html/base.tmpl.html",
-			"./cmd/web/ui/html/partials/nav.tmpl.html",
-			page,
-		}
-
-		ts, err := template.ParseFiles(files...)
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./cmd/web/ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
 
+		// Call ParseGlob() on the current template set to add partials.
+		ts, err = ts.ParseGlob("./cmd/web/ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() on the current template set to add the page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the current template set to the cache.
 		cache[baseName] = ts
 	}
 
