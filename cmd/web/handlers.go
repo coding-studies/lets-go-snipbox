ab54827c7d01b18f07d83a51a15a094517c56510
Fernando Basso <fernandobasso.br@gmail.com>
2025-02-03 10:11:11 -0300

    Handle new snippet submission
    • Apply basic validation on the input data.
    • Save to DB if all is fine.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -57,7 +58,27 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 // snippetCreatePost processes the post request to create a new snippet.
-func (app *application) snippetCreatePost(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save app new snippet"))
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
